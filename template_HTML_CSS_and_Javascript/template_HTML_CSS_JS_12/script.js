7c34a4844e7803749a28c32963679430cce072ec
VenusakaVXT <voxuantuan33@gmail.com>
2023-09-27 20:35:41 +0700

    Edit template_HTML_CSS_JS_12

--- template_HTML_CSS_and_Javascript/template_HTML_CSS_JS_12/script.js
+++ template_HTML_CSS_and_Javascript/template_HTML_CSS_JS_12/script.js
@@ -42,16 +42,42 @@ fetch('https://fakestoreapi.com/products')
 })
 
 const searchInput = document.querySelector('.search input');
+const productList = document.querySelector('.list-product');
 
 searchInput.addEventListener('input', (e) => {
     const txtSearch = e.target.value.trim().toLowerCase();
     const listProductDOM = document.querySelectorAll('.product-item');
 
     listProductDOM.forEach(item => {
-        if (item.innerText.toLowerCase().includes(txtSearch)) {
+        const productName = item.querySelector('.product-name');
+        const productPrice = item.querySelector('.product-price');
+
+        const nameText = productName.innerText.toLowerCase();
+        const priceText = productPrice.innerText.toLowerCase();
+
+        const highlightedName = nameText.replace(new RegExp(txtSearch, 'gi'), (match) => `<span class="highlighted">${match}</span>`);
+        const highlightedPrice = priceText.replace(new RegExp(txtSearch, 'gi'), (match) => `<span class="highlighted">${match}</span>`);
+
+        productName.innerHTML = highlightedName;
+        productPrice.innerHTML = highlightedPrice;
+
+        if (nameText.includes(txtSearch) || priceText.includes(txtSearch)) {
             item.classList.remove('hide');
         } else {
             item.classList.add('hide');
         }
-    })
+    });
 })
+
+// searchInput.addEventListener('input', (e) => {
+//     const txtSearch = e.target.value.trim().toLowerCase();
+//     const listProductDOM = document.querySelectorAll('.product-item');
+
+//     listProductDOM.forEach(item => {
+//         if (item.innerText.toLowerCase().includes(txtSearch)) {
+//             item.classList.remove('hide');
+//         } else {
+//             item.classList.add('hide');
+//         }
+//     })
+// })
